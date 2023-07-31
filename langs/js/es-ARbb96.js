e112843303bd04136722c6d2a9bb48784cb51945
Jere Herrero <103390914+JereHerrero@users.noreply.github.com>
2023-07-31 18:13:18 -0300

    Main 1.0

---
+++ langs/js/es-ARbb96.js
@@ -0,0 +1,29 @@
+// Form contacto
+
+var Lang_Form_Contacto_1 = 'Enviando mensaje...';
+var Lang_Form_Contacto_2 = 'No se ha podido enviar, intente más tarde';
+var Lang_Form_Contacto_3 = 'Mensaje recibido, en breve entraremos en contacto';
+
+
+var Lang_Form_Contacto_4 = 'Ingrese nombre';
+var Lang_Form_Contacto_5 = 'Ingrese teléfono o whatsapp';
+var Lang_Form_Contacto_6 = 'Ingrese email válido';
+var Lang_Form_Contacto_7 = 'Ingrese email';
+var Lang_Form_Contacto_8 = 'Ingrese su mensaje';
+
+var Lang_Form_Contacto_9 = 'Enviar mensaje';
+
+
+// Form solicitar invitacion
+var Lang_Form_Solicitar_Inv_1 = 'Solicitando invitación...';
+var Lang_Form_Solicitar_Inv_2 = 'No se ha podido solicitar, intente más tarde';
+var Lang_Form_Solicitar_Inv_3 = 'Solicitud recibida, en breve entraremos en contacto';
+
+
+var Lang_Form_Solicitar_Inv_4 = 'Ingrese nombre';
+var Lang_Form_Solicitar_Inv_5 = 'Ingrese teléfono o whatsapp';
+var Lang_Form_Solicitar_Inv_6 = 'Ingrese email válido';
+var Lang_Form_Solicitar_Inv_7 = 'Ingrese email';
+
+var Lang_Form_Solicitar_Inv_7 = 'Solicitar ahora';
+
